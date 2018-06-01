f91f538b7cb2c4cb382029c71dfd52f7b964e0d1
thinkocapo <thinkocapo@gmail.com>
2018-06-01 11:34:02 -0400

    update

--- run-front-and-back-end-apps.sh
+++ run-front-and-back-end-apps.sh
@@ -15,7 +15,9 @@ send-keys -t backend_app 'npm start' C-m
 tmux new-session -s workspace_name -n window_1_name -c ~/Projects/frontend_app \; send-keys -t window_1_name 'npm start' C-m \; new-window -n backend_app -c ~/Projects/backend_app \; send-keys -t backend_app 'npm start' C-m
 
 # And the window names aren't really visible when you do ctrl+b s to view open sessions/windows,
-# but we need them for send-keys to work (needs to know which window to send the keys too)
+# but we need them for send-keys to work (needs to know which window to send the keys too),
+# so try without them, and assume window names are defaulted as 0, 1
+tmux new-session -s workspace_name -c ~/Projects/frontend_app \; send-keys -t 0 'npm start' C-m \; new-window -c ~/Projects/backend_app \; send-keys -t 1 'npm start' C-m
 
 # Run each app in a seaparate Pane within 1 window, in a session called 'work'
 #tmux new-session -s work -n claimcdr -c ~/Projects/claimcdr \; send-keys -t claimcdr 'npm start' C-m \; split-window -v -c ~/Projects/backend-api
